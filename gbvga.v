bab9a992bddaf39fd078f2580c6a70f6e77a4dc4
Antti Huhtala <antti.huhtala@iki.fi>
2019-11-13 00:50:24 +0200

    Sync logic initial

--- gbvga.v
+++ gbvga.v
@@ -19,4 +19,31 @@ module gbvga(
 		.c0(pllclk),
 		.locked(locked)
 	);
+	
+	reg[10:0] h_counter;
+	reg[9:0] v_counter;
+	
+	always @(posedge pllclk)
+	begin
+		if(h_counter < 1055)
+		begin
+			h_counter <= h_counter+1;
+		end
+		else
+		begin
+			h_counter <= 0;
+			
+			if(v_counter < 627)
+			begin
+				v_counter <= v_counter+1;
+			end
+			else
+			begin
+				v_counter <= 0;
+			end
+		end
+	end
+	
+	assign hsync = (h_counter >= 840 && h_counter < 968);
+	assign vsync = (v_counter >= 601 && v_counter < 605);
 endmodule
