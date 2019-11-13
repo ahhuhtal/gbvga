6d9c68c705563dff9dc2d34213129caa0f2dc350
Antti Huhtala <antti.huhtala@iki.fi>
2019-11-13 21:29:45 +0200

    Registered output with one cycle delay

--- gbvga.v
+++ gbvga.v
@@ -1,11 +1,11 @@
 module gbvga(
 	input clk,
 	output pllclk_out,
-	output vsync,
-	output hsync,
-	output[1:0] r,
-	output[1:0] g,
-	output[1:0] b,
+	output reg vsync,
+	output reg hsync,
+	output reg[1:0] r,
+	output reg[1:0] g,
+	output reg[1:0] b,
 	output[7:0] segment,
 	output[3:0] digit);
 
@@ -46,13 +46,15 @@ module gbvga(
 	reg[10:0] h_counter;
 	reg[9:0] v_counter;	
 
+	wire hsync_next;
+	wire vsync_next;
+	wire[1:0] r_next;
+	wire[1:0] g_next;
+	wire[1:0] b_next;
+	
 	wire[14:0] data_address;
-	wire visible;
 	wire[1:0] data;
-	
-	assign data_address[14:0] = visible*(v_counter[9:2]*160 + h_counter[10:2]);
-	
-	assign data[1:0] = data_address[10:9];
+	wire visible;
 	
 	always @(posedge pllclk)
 	begin
@@ -73,12 +75,23 @@ module gbvga(
 				v_counter <= 0;
 			end
 		end
+		
+		vsync <= vsync_next;
+		hsync <= hsync_next;
+		r <= r_next;
+		g <= g_next;
+		b <= b_next;
 	end
 	
-	assign hsync = (h_counter >= h_vis + h_fp && h_counter < h_vis + h_fp + h_sync);
-	assign vsync = (v_counter >= v_vis + v_fp && v_counter < v_vis + v_fp + v_sync);
-
 	assign visible = h_counter < h_vis && v_counter < v_vis;
-	
-	assign r[1:0] = data[1:0] & visible;
+
+	assign data_address[14:0] = visible*(v_counter[9:2]*160 + h_counter[10:2]);
+
+	assign data[1:0] = data_address[10:9];
+
+	assign hsync_next = (h_counter >= h_vis + h_fp && h_counter < h_vis + h_fp + h_sync);
+	assign vsync_next = (v_counter >= v_vis + v_fp && v_counter < v_vis + v_fp + v_sync);
+	assign r_next[1:0] = data[1:0] & visible;
+	assign g_next[1:0] = data[1:0] & visible;
+	assign b_next[1:0] = data[1:0] & visible;
 endmodule
