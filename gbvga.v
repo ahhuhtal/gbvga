b6b73b40acdb6a2c67baba321f0fd3d0b366d921
Antti Huhtala <antti.huhtala@iki.fi>
2019-12-10 19:53:14 +0200

    Full screen with half resolution. Small blur computation pipeline change.

--- gbvga.v
+++ gbvga.v
@@ -25,14 +25,14 @@ module gbvga(
 	
 	// VGA output variables
 	
-	// params for 640x288 following 800x600 timings @ 40 MHz
+	// params for 640x576 following 800x600 timings @ 40 MHz
 	localparam h_vis = 640; // visible area horizontal pixels
 	localparam h_fp = 120; // horizontal front porch pixels
 	localparam h_sync = 128; // horizontal sync active pixels
 	localparam h_bp = 168; // horizontal back porch pixels
 
-	localparam v_vis = 288; // visible area vertical pixels
-	localparam v_fp = 301; // vertical front porch pixels
+	localparam v_vis = 576; // visible area vertical pixels
+	localparam v_fp = 13; // vertical front porch pixels
 	localparam v_sync = 4; // vertical sync active pixels
 	localparam v_bp = 35; // vertical back porch pixels
 
@@ -129,7 +129,7 @@ module gbvga(
 	
 	reg iwrite_enabled;
 	
-	reg[2:0] iwrite_state;
+	reg[1:0] iwrite_state;
 	
 
 	// 2-port RAM instantiation for framebuffer
@@ -198,93 +198,16 @@ module gbvga(
 				iwrite_enabled <= 0;
 			end
 
-			1: begin // start read
+			1: begin // old pixel data read, write data on next clock pulse
 				iwrite_state <= 2;
-				iwrite_enabled <= 0;
-			end
-			
-			2: begin // old pixel state was read
-				case({idata_oldstate, idata_latched})
-					6'b000000: idata_newstate <= 4'b0000;
-					6'b000001: idata_newstate <= 4'b0001;
-					6'b000010: idata_newstate <= 4'b0010;
-					6'b000011: idata_newstate <= 4'b0011;
-					6'b000100: idata_newstate <= 4'b0000;
-					6'b000101: idata_newstate <= 4'b0010;
-					6'b000110: idata_newstate <= 4'b0011;
-					6'b000111: idata_newstate <= 4'b0100;
-					6'b001000: idata_newstate <= 4'b0001;
-					6'b001001: idata_newstate <= 4'b0011;
-					6'b001010: idata_newstate <= 4'b0100;
-					6'b001011: idata_newstate <= 4'b0101;
-					6'b001100: idata_newstate <= 4'b0010;
-					6'b001101: idata_newstate <= 4'b0100;
-					6'b001110: idata_newstate <= 4'b0101;
-					6'b001111: idata_newstate <= 4'b0110;
-					6'b010000: idata_newstate <= 4'b0011;
-					6'b010001: idata_newstate <= 4'b0101;
-					6'b010010: idata_newstate <= 4'b0101;
-					6'b010011: idata_newstate <= 4'b0110;
-					6'b010100: idata_newstate <= 4'b0100;
-					6'b010101: idata_newstate <= 4'b0101;
-					6'b010110: idata_newstate <= 4'b0110;
-					6'b010111: idata_newstate <= 4'b0111;
-					6'b011000: idata_newstate <= 4'b0101;
-					6'b011001: idata_newstate <= 4'b0101;
-					6'b011010: idata_newstate <= 4'b0111;
-					6'b011011: idata_newstate <= 4'b1000;
-					6'b011100: idata_newstate <= 4'b0101;
-					6'b011101: idata_newstate <= 4'b0110;
-					6'b011110: idata_newstate <= 4'b1000;
-					6'b011111: idata_newstate <= 4'b1001;
-					6'b100000: idata_newstate <= 4'b0110;
-					6'b100001: idata_newstate <= 4'b0111;
-					6'b100010: idata_newstate <= 4'b1001;
-					6'b100011: idata_newstate <= 4'b1010;
-					6'b100100: idata_newstate <= 4'b0111;
-					6'b100101: idata_newstate <= 4'b1000;
-					6'b100110: idata_newstate <= 4'b1010;
-					6'b100111: idata_newstate <= 4'b1010;
-					6'b101000: idata_newstate <= 4'b1000;
-					6'b101001: idata_newstate <= 4'b1001;
-					6'b101010: idata_newstate <= 4'b1010;
-					6'b101011: idata_newstate <= 4'b1011;
-					6'b101100: idata_newstate <= 4'b1001;
-					6'b101101: idata_newstate <= 4'b1010;
-					6'b101110: idata_newstate <= 4'b1010;
-					6'b101111: idata_newstate <= 4'b1100;
-					6'b110000: idata_newstate <= 4'b1001;
-					6'b110001: idata_newstate <= 4'b1010;
-					6'b110010: idata_newstate <= 4'b1011;
-					6'b110011: idata_newstate <= 4'b1101;
-					6'b110100: idata_newstate <= 4'b1010;
-					6'b110101: idata_newstate <= 4'b1011;
-					6'b110110: idata_newstate <= 4'b1100;
-					6'b110111: idata_newstate <= 4'b1110;
-					6'b111000: idata_newstate <= 4'b1011;
-					6'b111001: idata_newstate <= 4'b1100;
-					6'b111010: idata_newstate <= 4'b1101;
-					6'b111011: idata_newstate <= 4'b1111;
-					6'b111100: idata_newstate <= 4'b1100;
-					6'b111101: idata_newstate <= 4'b1101;
-					6'b111110: idata_newstate <= 4'b1110;
-					6'b111111: idata_newstate <= 4'b1111;
-				endcase
-				
-				iwrite_state <= 3;
-				iwrite_enabled <= 0;
-			end
-			
-			3: begin // new value is computed
-				iwrite_state <= 4;
 				iwrite_enabled <= 1;
 			end
 			
-			4: begin // write completed
+			2: begin // data was written
 				iwrite_state <= 0;
 				iwrite_enabled <= 0;
 			end
-			
+						
 			default: begin
 				iwrite_state <= 0;
 				iwrite_enabled <= 0;
@@ -306,11 +229,11 @@ module gbvga(
 			if(ihsync_state == 0) begin
 				ipixel <= ipixel+1'd1; // increment pixel count
 				
-				if(ipixel < 11520) begin // only 11520 pixels of memory
+				if(ipixel[0] == 0) begin // store every other pixel, as there isn't enough memory for every pixel
 					// store the current pixel address as write address
 					// take data from a few clock cycles ago
 					// initiate write
-					ipixel_latched <= ipixel;
+					ipixel_latched <= ipixel[14:1];
 					idata_latched <= ~idata_prev5;
 					iwrite_state <= 1;
 				end
@@ -331,11 +254,11 @@ module gbvga(
 
 			ipixel <= ipixel+1'd1; // increment pixel count
 
-			if(ipixel < 11520) begin // only 11520 pixels of memory
+			if(ipixel[0] == 0) begin // store every other pixel, as there isn't enough memory for every pixel
 				// store the current pixel address
 				// take data from a few clock cycles ago
 				// initiate write
-				ipixel_latched <= ipixel;
+				ipixel_latched <= ipixel[14:1];
 				idata_latched <= ~idata_prev5;
 				iwrite_state <= 1;
 			end
@@ -386,6 +309,76 @@ module gbvga(
 		idata_prev5 <= idata_prev4;
 	end
 	
+    // compute motion blur
+    always @* begin
+	    case({idata_oldstate, idata_latched})
+            6'b000000: idata_newstate <= 4'b0000;
+            6'b000001: idata_newstate <= 4'b0010;
+            6'b000010: idata_newstate <= 4'b0011;
+            6'b000011: idata_newstate <= 4'b0101;
+            6'b000100: idata_newstate <= 4'b0000;
+            6'b000101: idata_newstate <= 4'b0010;
+            6'b000110: idata_newstate <= 4'b0100;
+            6'b000111: idata_newstate <= 4'b0110;
+            6'b001000: idata_newstate <= 4'b0001;
+            6'b001001: idata_newstate <= 4'b0011;
+            6'b001010: idata_newstate <= 4'b0101;
+            6'b001011: idata_newstate <= 4'b0110;
+            6'b001100: idata_newstate <= 4'b0010;
+            6'b001101: idata_newstate <= 4'b0100;
+            6'b001110: idata_newstate <= 4'b0101;
+            6'b001111: idata_newstate <= 4'b0111;
+            6'b010000: idata_newstate <= 4'b0011;
+            6'b010001: idata_newstate <= 4'b0101;
+            6'b010010: idata_newstate <= 4'b0110;
+            6'b010011: idata_newstate <= 4'b1000;
+            6'b010100: idata_newstate <= 4'b0011;
+            6'b010101: idata_newstate <= 4'b0101;
+            6'b010110: idata_newstate <= 4'b0111;
+            6'b010111: idata_newstate <= 4'b1000;
+            6'b011000: idata_newstate <= 4'b0100;
+            6'b011001: idata_newstate <= 4'b0101;
+            6'b011010: idata_newstate <= 4'b0111;
+            6'b011011: idata_newstate <= 4'b1001;
+            6'b011100: idata_newstate <= 4'b0101;
+            6'b011101: idata_newstate <= 4'b0110;
+            6'b011110: idata_newstate <= 4'b1000;
+            6'b011111: idata_newstate <= 4'b1010;
+            6'b100000: idata_newstate <= 4'b0101;
+            6'b100001: idata_newstate <= 4'b0111;
+            6'b100010: idata_newstate <= 4'b1001;
+            6'b100011: idata_newstate <= 4'b1010;
+            6'b100100: idata_newstate <= 4'b0110;
+            6'b100101: idata_newstate <= 4'b1000;
+            6'b100110: idata_newstate <= 4'b1010;
+            6'b100111: idata_newstate <= 4'b1011;
+            6'b101000: idata_newstate <= 4'b0111;
+            6'b101001: idata_newstate <= 4'b1000;
+            6'b101010: idata_newstate <= 4'b1010;
+            6'b101011: idata_newstate <= 4'b1100;
+            6'b101100: idata_newstate <= 4'b0111;
+            6'b101101: idata_newstate <= 4'b1001;
+            6'b101110: idata_newstate <= 4'b1010;
+            6'b101111: idata_newstate <= 4'b1100;
+            6'b110000: idata_newstate <= 4'b1000;
+            6'b110001: idata_newstate <= 4'b1010;
+            6'b110010: idata_newstate <= 4'b1011;
+            6'b110011: idata_newstate <= 4'b1101;
+            6'b110100: idata_newstate <= 4'b1001;
+            6'b110101: idata_newstate <= 4'b1010;
+            6'b110110: idata_newstate <= 4'b1100;
+            6'b110111: idata_newstate <= 4'b1110;
+            6'b111000: idata_newstate <= 4'b1001;
+            6'b111001: idata_newstate <= 4'b1011;
+            6'b111010: idata_newstate <= 4'b1101;
+            6'b111011: idata_newstate <= 4'b1111;
+            6'b111100: idata_newstate <= 4'b1010;
+            6'b111101: idata_newstate <= 4'b1100;
+            6'b111110: idata_newstate <= 4'b1101;
+            6'b111111: idata_newstate <= 4'b1111;
+        endcase
+    end
+			
 	// assign output
 	
 	// compute the visibility signal for the pixel after 2 clock cycles
@@ -394,7 +387,7 @@ module gbvga(
 	// compute the address in framebuffer for the pixel after 2 clock cycles
 	// if pixel is not visible, default to address 0
 	// otherwise address = vcount/4 * 160 + hcount/4
-	assign opixel_next2[14:0] = visible_next2*(vcounter_next2[9:2]*8'd160 + hcounter_next2[10:2]);
+	assign opixel_next2[14:0] = visible_next2*(vcounter_next2[9:2]*8'd80 + hcounter_next2[10:3]);
 	
 	// compute hsync and vsync signals for the pixel after 2 clock cycles
 	// polarity is positive for the svga 800x600
